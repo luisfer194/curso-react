65ed3ab255cb198898e5771128ab2a77020d33a4
luisfer194 <luissfer194@gmail.com>
2026-01-19 20:20:44 -0500

    add frist class

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,17 +1,53 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
- 
+const users = [
+  {
+    userName: 'luisfer194',
+    name: 'Luis Fernando',
+    isFollowing: true
+  },
+  {
+    userName: 'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: false
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hernandez',
+    isFollowing: true
+  },
+  {
+    userName: 'juandc',
+    name: 'Juan David C',
+    isFollowing: false
+  },
+  {
+    userName: 'carlogilmar',
+    name: 'Carlo Gilmar',
+    isFollowing: true
+  }
+]
 
 export function App() {
   return (
     <section className='App'>
-      <TwitterFollowCard userName="luisfer194" initialIsFollowing={true}>
-        Luis Fernando
-      </TwitterFollowCard>
-      <TwitterFollowCard userName="midudev" initialIsFollowing={false}>
-      Miguel Angel Duran
-    </TwitterFollowCard>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
     </section >
   )
 }
